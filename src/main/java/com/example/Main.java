58893a00ab43d3aff093c39665e05d66f6bd7b60
Oscar Nidemar <oscar.nidemar@gmail.com>
2025-10-03 09:25:09 +0200

    Nästan där

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,9 +1,29 @@
 package com.example;
 
+import com.example.api.ElprisCLI;
 import com.example.api.ElpriserAPI;
+import picocli.CommandLine;
 
 public class Main {
+
+
+    public static int run(String[] args) {
+        ElpriserAPI api = new ElpriserAPI();
+        ElprisCLI cli = new ElprisCLI(api);
+        return new CommandLine(cli).execute(args);
+    }
+
     public static void main(String[] args) {
-        ElpriserAPI elpriserAPI = new ElpriserAPI();
+        int exitCode = run(args);
+
+
+        if (!isRunningInTestEnvironment()) {
+            System.exit(exitCode);
+        }
+    }
+
+    // Identifiera testmiljö via systemproperty som vi sätter i Maven
+    private static boolean isRunningInTestEnvironment() {
+        return Boolean.getBoolean("test.environment");
     }
 }
